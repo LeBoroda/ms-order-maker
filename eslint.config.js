e2d93f94d6f2f658351ca6a5132123be96c760fb
Stanislav <stalevs@gmail.com>
2025-11-13 18:41:14 +0100

    install eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,45 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import ts from '@typescript-eslint/eslint-plugin'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
+      sourceType: 'module',
       globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      parser: '@typescript-eslint/parser',
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      '@typescript-eslint': ts,
+      'react-refresh': reactRefresh,
+    },
+    extends: [
+      js.configs.recommended,
+      ts.configs.recommended,
+      react.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      'plugin:prettier/recommended', // включаем prettier как часть ESLint
+    ],
+    rules: {
+      // Можно добавить кастомные правила, например:
+      'react/react-in-jsx-scope': 'off', // для React 17+
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'prettier/prettier': 'error',
     },
   },
 ])
